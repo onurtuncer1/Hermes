f566e6400f135ca5ee965224d9b8549ecee276d4
Onur Tuncer <onur.tuncer@melina-aero.com>
2025-08-06 17:35:20 +0300

    changed include dir for cppcheck

--- cmake/cppcheck.cmake
+++ cmake/cppcheck.cmake
@@ -5,34 +5,29 @@
 # License-Filename: LICENSE
 # ------------------------------------------------------------------------------
 
-# Check prerequisites
 FIND_PROGRAM(CPPCHECK_PATH cppcheck)
 
 IF(NOT CPPCHECK_PATH)
     MESSAGE(FATAL_ERROR "cppcheck not found! Aborting...")
 ENDIF()
 
-# Gather source files
 file(GLOB_RECURSE APP_SOURCES "${CMAKE_SOURCE_DIR}/src/*.cpp")
 
-# Set directories for analysis
-set(STATIC_ANALYSIS_SOURCE_DIR ${CMAKE_SOURCE_DIR})  # Updated to CMAKE_SOURCE_DIR
+set(STATIC_ANALYSIS_SOURCE_DIR ${CMAKE_SOURCE_DIR}) 
 set(STATIC_ANALYSIS_REPORT_DIR ${CMAKE_BINARY_DIR}/static_analysis)
 file(MAKE_DIRECTORY ${STATIC_ANALYSIS_REPORT_DIR})
 set(STATIC_ANALYSIS_XML_FILE ${STATIC_ANALYSIS_REPORT_DIR}/static_analysis_report.xml)
 
-# Add namespaced custom target for static analysis (using CMake project name)
 set(NAMESPACED_TARGET "static_analysis")
 add_custom_target(${NAMESPACED_TARGET})
 
-# Add custom command to run cppcheck and generate the report
 add_custom_command(TARGET ${NAMESPACED_TARGET}
   COMMAND cppcheck
    ARGS  --enable=all
          --force
          --suppress=unusedFunction
          --cppcheck-build-dir=${STATIC_ANALYSIS_REPORT_DIR}
-         -I/${CMAKE_SOURCE_DIR}/${CMAKE_PROJECT_NAME}
+         -I/${CMAKE_SOURCE_DIR}
          --suppressions-list=${CMAKE_BINARY_DIR}/../../tools/cppcheck/CppCheckSuppressions.txt
          --inconclusive
          --xml-version=2
